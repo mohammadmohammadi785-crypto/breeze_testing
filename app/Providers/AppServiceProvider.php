1f05ceb31e28b99e00e24f29a446ff0d6985b2e2
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-18 16:14:26 +0430

    Changes added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Gate::define('teacher-auth', function (User $user){
+            return $user->role === 'teacher';
+        });
     }
 }

84f94dab50d0a85f895c7516f636bb8c03b732bf
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-19 21:49:20 +0430

    Changes added

--- resources/views/Post/create.blade.php
+++ resources/views/Post/create.blade.php
@@ -12,10 +12,10 @@
             <h1 class="text-2xl text-center font-black">Post Create</h1>
             <form action="{{ url('/create-post') }}" enctype="multipart/form-data" method="post" class="w-full flex flex-col gap-2.5">
                 @csrf
-                <input class="py-3 px-2 w-full border rounded-md focus:outline-0" name="title" type="text">
-                <input class="py-3 px-2 w-full border rounded-md focus:outline-0" name="body" type="text">
-                <input class="py-3 px-2 w-full border rounded-md focus:outline-0" name="image1" type="file" accept="image/*">
-                <input class="py-3 px-2 w-full border rounded-md focus:outline-0" name="image2" type="file" accept="image/*">
+                <input class="py-3 px-2 bg-black w-full border rounded-md focus:outline-0" name="title" type="text">
+                <input class="py-3 px-2 bg-black w-full border rounded-md focus:outline-0" name="body" type="text">
+                <input class="py-3 px-2 bg-black w-full border rounded-md focus:outline-0" name="image1" type="file" accept="image/*">
+                <input class="py-3 px-2 bg-black w-full border rounded-md focus:outline-0" name="image2" type="file" accept="image/*">
                 <input type="submit" class="w-full py-2 border rounded-md" value="Save">
             </form>
         </div>

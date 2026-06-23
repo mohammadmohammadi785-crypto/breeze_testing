babe42ea3a73d93c4a5105affb7cab4b73331530
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-23 21:15:09 +0430

    Changes added

--- app/Http/Middleware/TeacherMiddleware.php
+++ app/Http/Middleware/TeacherMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class TeacherMiddleware
@@ -15,6 +16,10 @@ class TeacherMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $teacher = Auth::user();
+        if (!$teacher || $teacher->role !== 'teacher') {
+            return back();
+        }
         return $next($request);
     }
 }

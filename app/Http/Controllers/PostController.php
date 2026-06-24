2481d69712e99f6082d540ad43750a7c581cab0d
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-24 21:53:09 +0430

    Changes added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,64 +1,74 @@
 <?php
-namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
+use App\Models\Post;    
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
-use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(){
-        $posts = Post::with('images')->get();
-        return view('Post.index')->with("posts", $posts);
-    }
-    public function create(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $request->validate([
-            "title"=> "required|string|min:3",
-            "body"=> "required|string|min:10",
-            "image1"=> "required|image|mimes:png,jpg,jpeg,svg,gif",
-            "image2"=> "required|image|mimes:png,jpg,jpeg,svg,gif",
-        ]);
-        $post = Post::create([
-            "title"=> $request->title,
-            "body"=> $request->body,
-        ]);
-        $imageUrl1 = null;
-        $imageUrl2 = null;
-        if($request->hasFile("image1") && $request->hasFile("image2")){
-            $imageUrl1 = $request->file("image1")->store("post_images", "public");
-            $imageUrl2 = $request->file("image2")->store("post_images", "public");
-            $post->images()->createMany(
-                [
-                    ["image_url"=>$imageUrl1],
-                    ["image_url"=>$imageUrl2],
-                ]
-            );
+        Gate::authorize('view');
+        if(Gate::allows('view')){
+           $posts = Post::all();
+           return response()->json([
+            "data" => $posts,
+           ]);
         }
-        return redirect('/Post');
+        else{
+            abort(403);
+        }
     }
 
-    public function edit(String $id){
-        $post = Post::findOrFail($id);
-        return view('Post.update')->with('post', $post);
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
     }
-    public function update(Request $request, String $id){
-        $post = Post::findOrFail($id);
-        $request->validate([
-            "title"=> "required|string",
-            "body"=> "required|string",     
-            // "image"=> "required|image|mimes:jpg,png,gif,jpeg,svg"
-        ]);
-        // $image_url = null;
-        // if($request->hasFile('image')){
-        //     if(Storage::disk('public')->exists($post->image)){
-        //         Storage::disk('public')->delete($post->image);
-        //         $image_url = $request->file('image')->store('post_images', "public");
-        //     }
-        // }
-        $post->update([
-            "title"=> $request->title,
-            "body"=>$request->body,
-        ]);
-        return redirect('/Post');
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }

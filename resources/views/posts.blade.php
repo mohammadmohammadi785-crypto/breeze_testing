26438005b7868dc74a9215e28b3b97a78d2c28aa
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-07-02 16:51:24 +0430

    Changes added

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,3 +1,44 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite("resources/css/app.css")
+</head>
+<body>
+    <div class="w-full min-h-screen bg-stone-100">
+        <div class="w-full max-w-7xl mx-auto p-4">
+            <div class="">
+                <form method="post">
+                    <input type="text">
+                </form>
+            </div>
+            @if (count($posts) > 0)
+            <table class="w-full border-2">
+                <tr>
+                    <th class="border py-2 text-center">Post title</th>
+                    <th class="border py-2 text-center">Post description</th>
+                    <th class="border py-2 text-center">Update</th>
+                    <th class="border py-2 text-center">Delete</th>
+                </tr>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td class="border py-2 text-center">{{ $post->title }}</td>
+                        <td class="border py-2 text-center">{{ $post->body }}</td>
+                        @can('update')
+                        <td class="border py-2 text-center">
+                            <a href="" class="bg-blue-500 text-white px-4 py-2 rounded-md">Update</a>
+                        </td>
+                        @endcan
+                        <td class="border py-2 text-center">
+                            <a href="" class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+            @endif
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get("posts", [PostController::class, "index"]);
 });
 
 Route::get('user', [UserController::class, "index"])->middleware('can:teacher-auth');
+
 
 // Route::get("teacher", [TeacherController::class, "index"])->middleware(TeacherMiddleware::class);
 
